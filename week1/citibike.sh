74fe5899c1265de0ed3bc15147383bf9b875bf33
GabyMartinez <gmg.j04@gmail.com>
2020-06-02 16:40:58 -0400

    solutions

--- week1/citibike.sh
+++ week1/citibike.sh
@@ -3,21 +3,49 @@
 # add your solution after each of the 10 comments below
 #
 
-# count the number of unique stations
+echo count the number of unique stations
+    cut -d, -f5 201402-citibike-tripdata.csv | sort | uniq | wc -l    
 
-# count the number of unique bikes
+    #330
+    #subtract 1 because it counts the header as a unique "station" 
+    #329
 
-# count the number of trips per day
+echo count the number of unique bikes
+    cut -d, -f12 201402-citibike-tripdata.csv | sort | uniq | wc -l
 
-# find the day with the most rides
+    #5700
+    #based on bikeID
 
-# find the day with the fewest rides
-
-# find the id of the bike with the most rides
-
-# count the number of rides by gender and birth year
-
-# count the number of trips that start on cross streets that both contain numbers (e.g., "1 Ave & E 15 St", "E 39 St & 2 Ave", ...)
+echo count the number of trips per day
+    cut -d, -f2 201402-citibike-tripdata.csv | sort | cut -c -11 | uniq -c
 
 
-# compute the average trip duration
+echo find the day with the most rides
+    cut -d, -f2 201402-citibike-tripdata.csv | sort | cut -c -11 | uniq -c | sort
+    
+    #13816 2014-02-02
+    
+echo find the day with the fewest rides
+    cut -d, -f2 201402-citibike-tripdata.csv | sort | cut -c -11 | uniq -c | sort
+
+    # 876 2014-02-13
+
+echo find the id of the bike with the most rides
+    cut -d, -f12 201402-citibike-tripdata.csv | sort | uniq -c |sort
+
+   # 130 "20837"    
+
+echo count the number of rides by gender and birth year
+    cut -d, -f14-15 201402-citibike-tripdata.csv | sort | uniq -c
+
+echo count the number of trips that start on cross streets that both contain numbers (e.g., "1 Ave & E 15 St", "E 39 St & 2 Ave", ...)
+    cut -d, -f5 201402-citibike-tripdata.csv | grep '[0-9].* & .*[0-9]' | sort | uniq -c | awk '{s+=$1}END{print s}'
+    
+    #90549
+
+echo compute the average trip duration
+    cut -d, -f1 201402-citibike-tripdata.csv | tr -d '"' | awk '{s+=$1}END{print s/224736}'
+
+    #874.52
+
+
