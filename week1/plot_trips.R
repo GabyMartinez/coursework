d7d2e40677a3399afeb2eac1a434f64dcea172f0
GabyMartinez <37944662+GabyMartinez@users.noreply.github.com>
2020-06-08 18:37:28 -0400

    update

--- week1/plot_trips.R
+++ week1/plot_trips.R
@@ -68,7 +68,10 @@ weather %>% ggplot(aes(x = date, y = tmin)) + geom_point()
 # hint: try using the gather() function for this to reshape things before plotting
 # (you can skip this and come back to it tomorrow if we haven't covered gather() yet)
   
-  weather %>% select(ymd, tmax, tmin) %>% pivot_longer(names_to = "max_min", values_to = "temp", 2:3) %>% ggplot(aes(x=ymd, y=temp, color = max_min)) + geom_point()
+  weather %>% 
+    select(ymd, tmax, tmin) %>% 
+    pivot_longer(names_to = "max_min", values_to = "temp", 2:3) %>% 
+    ggplot(aes(x=ymd, y=temp, color = max_min)) + geom_point()
   
   
 ########################################
@@ -76,16 +79,31 @@ weather %>% ggplot(aes(x = date, y = tmin)) + geom_point()
 ########################################
 
 # join trips and weather
-trips_with_weather <- inner_join(trips, weather, by="ymd")
+  trips_with_weather <- left_join(trips, weather, by="ymd")
 
 # plot the number of trips as a function of the minimum temperature, where each point represents a day
 # you'll need to summarize the trips and join to the weather data to do this
 
+  trips_with_weather %>% group_by(tmin) %>% summarize(count=n()) -> temp
+  ggplot(temp, aes(x=tmin, y=count)) + geom_point()
   
 
 # repeat this, splitting results by whether there was substantial precipitation or not
 # you'll need to decide what constitutes "substantial precipitation" and create a new T/F column to indicate this
 
+  trips_with_weather %>% 
+    mutate(s_prcp = ifelse(prcp > 1.5, T, F)) %>% View()
+  
+  
+  #    if(prcp > 1.5){
+  #      substantial_prcp = TRUE}
+  #    else
+  #      substantial_prcp = FALSE
+  #      ) -> trips_with_weather
+  
+    ggplot(aes(x=tmin, color=substantial_prcp)) +
+    geom_histogram()
+  
 # add a smoothed fit on top of the previous plot, using geom_smooth
 
 # compute the average number of trips and standard deviation in number of trips by hour of the day
