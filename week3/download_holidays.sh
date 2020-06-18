021b85224933801bdfaf833db3771afc1d969660
GabyMartinez <37944662+GabyMartinez@users.noreply.github.com>
2020-06-18 18:49:59 -0400

    week 3 -day 4

---
+++ week3/download_holidays.sh
@@ -0,0 +1,11 @@
+#download 2012-2020 holiday data
+
+url=https://gist.github.com/shivaas/4758439/archive/b0d3ddec380af69930d0d67a9e0519c047047ff8.zip
+
+curl -o holiday.zip $url
+
+#unzip holiday.zip 
+
+#unzip -j holiday.zip > holiday.csv
+
+#rm holiday.zip
